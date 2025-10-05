547cdcd0018066bc6349acb03314dfa41763840a
Bhanu <bhanu@Bhanus-MacBook-Pro.local>
2025-10-05 18:37:27 +0530

    Second Commit

--- frontend-vanilla 21-41-31-193/assets/socket.js
+++
@@ -1,5 +0,0 @@
-// Requires <script src="http://localhost:4000/socket.io/socket.io.js"></script> on pages that need realtime
-function connectSocket() {
-    const host = API_BASE.replace('/api','');
-    return io(host, { transports: ['websocket'] });
-  }
